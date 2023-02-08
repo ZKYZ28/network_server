28abcd1aa58631e1cc09102627efdf293cdbaeb4
Anthony UCCI <a.ucci@student.helmo.be>
2023-02-08 20:36:54 +0100

    c1

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.helmo {
+    requires com.google.gson;
+    exports org.helmo.murmurG6;
+}
---
+++ app/src/main/java/org/helmo/murmurG6/app.java
@@ -0,0 +1,7 @@
+package org.helmo.murmurG6;
+
+public class app {
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
+}
