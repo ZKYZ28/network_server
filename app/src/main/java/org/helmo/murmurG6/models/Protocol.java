f0aecf99f361abbe74a2ac219858f6d1190f71ea
Edwin De Vlegelaer <e.devlegelaer@student.helmo.be>
2023-02-09 19:28:14 +0100

    Avancement grammaire

--- app/src/main/java/org/helmo/murmurG6/models/Protocol.java
+++ app/src/main/java/org/helmo/murmurG6/models/Protocol.java
@@ -8,20 +8,45 @@ import java.util.regex.Pattern;
 
 public class Protocol {
 
-    /*PARTS*/
+    private static final String RX_DIGIT = "[0-9]";
+    private static final String RX_LETTER = "[a-zA-Z]";
+    private static final String RX_LETTER_DIGIT = RX_LETTER + "|" + RX_DIGIT;
+    /*
+     * ACSII Characters
+     * [\x21-\x2f] : correspond à la plage des codes hexadécimaux 0x21 à 0x2f, qui comprend les symboles suivants :
+     * ! " # $ % & ' ( ) * + , - . /
+     *
+     * [\x3a-\x40] : correspond à la plage des codes hexadécimaux 0x3a à 0x40, qui comprend les symboles suivants :
+     * : ; < = > ? @
+     *
+     * [\x5B-\x60] : correspond à la plage des codes hexadécimaux 0x5B à 0x60, qui comprend les symboles suivants :
+     * [ \ ] ^ _ `
+     *
+     */
+    private static final String RX_SYMBOL = "[\\x21-\\x2f]|[\\x3a-\\x40]|[\\x5B-\\x60]";
+    private static final String RX_ROUND = "(" + RX_DIGIT + "{2})";
+    private static final String RX_PASSCHAR = "[\\x22-\\xff]";
+    private static final String RX_VISIBLE_CHARACTER = "[\\x20-\\xff]";
+    private static final String RX_INFORMATION_MESSAGE = "((" + RX_VISIBLE_CHARACTER + "){0,200})";
+    private static final String RX_RANDOM = "((" + RX_LETTER_DIGIT + "|" + RX_SYMBOL + "){22})";
+    private static final String RX_BCRYPT_SALT = "((" + RX_LETTER_DIGIT + "|" + RX_SYMBOL + "){22})";
     private final static String RX_ESP = "\\s";
-    private final static String RX_CRLF = "(\\r\\n)?";
-    private final static String RX_USERNAME = "([a-zA-Z]{5,20})$";
-    private final static String RX_DOMAIN = "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\\\.)+[A-Za-z]{2,6}$";
+    private static final String RX_DOMAIN = "((" + RX_LETTER_DIGIT + "|\\.){5,200})";
+    private static final String RX_USERNAME = "((" + RX_LETTER_DIGIT + "){5,20})";
+    private static final String RX_USER_DOMAIN = "(" + RX_USERNAME + "@" + RX_DOMAIN + ")";
+    private static final String RX_MESSAGE = "((" + RX_VISIBLE_CHARACTER + "){1,250})";
+
+    /*PARTS*/
+    private static final String RX_CRLF = "(\\x0d\\x0a){0,1}";
     private static final String RX_SALT_SIZE = "[0-9]{2}";
-    private static final String RX_BCRYPT_HASH = "^\\$2[ayb]\\$.{56}$";
+    private static final String RX_BCRYPT_HASH = "\\$2b\\$\\d{2}\\$(" + RX_LETTER_DIGIT + "|" + RX_SYMBOL + "){1,70}";
     private static final String TAG_DOMAIN = "#[a-zA-Z0-9]{5,20}";
 
 
     /*FULL*/
     private final static String CONNECT = "CONNECT" + RX_ESP + RX_USERNAME + RX_CRLF;
     private final static String REGISTER = "REGISTER" + RX_ESP + RX_USERNAME + RX_ESP + RX_SALT_SIZE + RX_ESP + RX_BCRYPT_HASH + RX_CRLF;
-    private final static String FOLLOW = "FOLLOW" + RX_ESP + RX_DOMAIN + TAG_DOMAIN + RX_CRLF;
+    private final static String FOLLOW = "FOLLOW" + RX_ESP + RX_DOMAIN + "|" + TAG_DOMAIN + RX_CRLF;
     private String TYPE_MESSAGE[] = {CONNECT, REGISTER, FOLLOW};
 
     public Message analyseMessage(String msg){
@@ -53,7 +78,7 @@ public class Protocol {
     }
 
     public static void main(String[] args) {
-        System.out.println(CONNECT);
+        System.out.println(RX_BCRYPT_HASH);
        System.out.println(Pattern.matches(CONNECT, "CONNECT Louis\\n\\r"));
     }
 }
--- app/src/test/java/org/helmo/murmurG6/models/ProtocolTest.java
+++ app/src/test/java/org/helmo/murmurG6/models/ProtocolTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ProtocolTest {
+
+class ProtocolTest {
 
     @Test
-    void checkConnect() {
+    void checkConnectInTheRightCase() {
         //Given
         Protocol protocol = new Protocol();
         String msgClientToServer = "CONNECT LOUIS\r\n";
@@ -16,5 +17,69 @@ public class ProtocolTest {
         Message connectMessage = protocol.analyseMessage(msgClientToServer);
         //Then
        assertEquals(MessageType.CONNECT, connectMessage.getType());
+    }
+
+    @Test
+    void checkConnectInBadCase() {
+        //Given
+        Protocol protocol = new Protocol();
+        String msgClientToServer = "CONNECT @@123abc\r\n";
+        //When
+        Message connectMessage = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.MESSAGE, connectMessage.getType());
+    }
+
+    @Test
+    void registerInTheRightCase() {
+        //Given
+        Protocol protocol = new Protocol();
+        String msgClientToServer = "REGISTER LOUIS 22 $2b$14$gfuefhsivdiv18\r\n";
+        //When
+        Message msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.REGISTER, msg.getType());
+
+        //When
+        msgClientToServer = "REGISTER LOUIS 22 $2b$16$gfuefhsivdiv18\r\n";
+        msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.REGISTER, msg.getType());
+
+        //When
+        msgClientToServer = "REGISTER LOUIS 22 $2b$14$grgsdgrgrgrvfc@()*+,-./\r\n";
+        msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.REGISTER, msg.getType());
+
+        //When
+        msgClientToServer = "REGISTER LOUIS 22 $2b$14$grgsdgrgrgrvfc@()*+,-./\r\n";
+        msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.REGISTER, msg.getType());
+    }
+
+    @Test
+    void registerInBadCase() {
+        //Given
+        Protocol protocol = new Protocol();
+        String msgClientToServer = "REGISTER LOUIS 1 $2b$14$trefvrgtgbgrvfcezf\r\n";
+        //When
+        Message msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.MESSAGE, msg.getType());
+
+        //When
+        msgClientToServer = "REGISTER LOUIS 22 $2b14$trarefefefzfzzf\r\n";
+        msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.MESSAGE, msg.getType());
+
+        //When
+        msgClientToServer = "REGISTER LOUIS 22 $3b$14$grgsdgrgrgrvfc\r\n";
+        msg = protocol.analyseMessage(msgClientToServer);
+        //Then
+        assertEquals(MessageType.MESSAGE, msg.getType());
+
     }
 }
