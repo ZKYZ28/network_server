88f0c1ede715be79abfba644ba08a480d05c584c
q210155 <l.loiseau@student.helmo.be>
2023-02-09 21:22:59 +0100

    Modifications ServerController

--- app/src/main/java/org/helmo/murmurG6/app.java
+++ app/src/main/java/org/helmo/murmurG6/app.java
@@ -1,12 +1,16 @@
 package org.helmo.murmurG6;
 
 import org.helmo.murmurG6.server.ServerController;
-import org.helmo.murmurG6.utils.RandomSaltGenerator;
+import java.io.IOException;
 
 public class app {
 
     private static final int DEFAULT_PORT = 12345;
-    public static void main(String[] args){
-        new ServerController(DEFAULT_PORT);
+    public static void main(String[] args) {
+        try (ServerController server = new ServerController(DEFAULT_PORT)) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
