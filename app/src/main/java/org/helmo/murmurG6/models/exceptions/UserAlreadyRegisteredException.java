7e0dedb3d6d15d98624414cc42d2723f1bf90975
q210155 <l.loiseau@student.helmo.be>
2023-02-09 18:31:29 +0100

    Création de la classe UserCollection.java

---
+++ app/src/main/java/org/helmo/murmurG6/models/exceptions/UserAlreadyRegisteredException.java
@@ -0,0 +1,8 @@
+package org.helmo.murmurG6.models.exceptions;
+
+public class UserAlreadyRegisteredException extends Exception {
+
+    public UserAlreadyRegisteredException(String msg) {
+        super(msg);
+    }
+}
