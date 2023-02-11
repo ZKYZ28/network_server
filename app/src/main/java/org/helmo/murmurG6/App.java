28009bf62122ba59ed02d949f3b22429a6f0de5a
Anthony UCCI <a.ucci@student.helmo.be>
2023-02-11 10:53:42 +0100

    register (bug save resolu2)

--- app/src/main/java/org/helmo/murmurG6/App.java
+++ app/src/main/java/org/helmo/murmurG6/App.java
@@ -4,6 +4,7 @@ import org.helmo.murmurG6.infrastructure.storage.json.UserJsonStorage;
 import org.helmo.murmurG6.models.User;
 import org.helmo.murmurG6.models.UserCollection;
 import org.helmo.murmurG6.repository.exceptions.SaveUserCollectionException;
+import org.helmo.murmurG6.system.ServerController;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,27 +16,11 @@ public class App {
     private static final UserJsonStorage USER_JSON_STORAGE = new UserJsonStorage();
 
 
-
-
-
     public static void main(String[] args) throws IOException {
-        /*try (ServerController server = new ServerController(DEFAULT_PORT, USER_JSON_STORAGE)) {
+        try (ServerController server = new ServerController(DEFAULT_PORT, USER_JSON_STORAGE)) {
             server.start();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
-
-        User u = new User("antho", "azerty", 14, "femme");
-
-        UserCollection uc = new UserCollection();
-        UserJsonStorage uj = new UserJsonStorage();
-
-
-
-        List<User> ul = new ArrayList<>();
-        ul.add(u);
-        uc.setRegisteredUsers(ul);
-
-        uj.save(uc.getRegisteredUsers());
+        }
     }
 }
