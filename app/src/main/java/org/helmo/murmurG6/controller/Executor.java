267a028c84dca3d9f5c78e68c9d94964b57d83a0
q210155 <l.loiseau@student.helmo.be>
2023-02-15 22:24:43 +0100

    Executor Refactored

--- app/src/main/java/org/helmo/murmurG6/controller/Executor.java
+++ app/src/main/java/org/helmo/murmurG6/controller/Executor.java
@@ -20,34 +20,28 @@ public class Executor implements TaskScheduler {
     private final BlockingQueue<Task> taskQueue; //File d'attente BlockingQueue appelée taskQueue pour stocker les tâches à exécuter.
     private ServerController server;
 
-    private Executor() {
+    private Executor () {
         this.taskQueue = new LinkedBlockingQueue<>();
         this.executorService = Executors.newSingleThreadExecutor();
     }
 
-    public static Executor getInstance() {
+    public static Executor getInstance(){
         if (instance == null) {
             instance = new Executor();
         }
         return instance;
     }
 
-    public void setServer(ServerController server) {
+    public void setServer(ServerController server){
         this.server = server;
     }
 
-    /**
-     * Permet d'ajouter des tâches à la file d'attente
-     *
-     * @param task La tâche a ajouter à la file d'attente
-     */
+
     public void addTask(Task task) {
-        this.taskQueue.add(task);
+        taskQueue.add(task);
     }
 
-    /**
-     * Fait tourner l'executor sur un thread
-     */
+
     @Override
     public void run() {
         executorService.submit(() -> {
@@ -63,6 +57,7 @@ public class Executor implements TaskScheduler {
         });
     }
 
+
     private void executeTask(Task task) {
 
         ClientRunnable client = task.getClient(); //On récupère le client à qui on fait la tache
@@ -70,25 +65,29 @@ public class Executor implements TaskScheduler {
         User user;
 
         switch (task.getType()) {
-            case REGISTER -> {
+            case REGISTER:
                 user = new User(params.group("username"), BCrypt.of(params.group("bcrypt")), new ArrayList<>(), new ArrayList<>());
                 client.sendMessage(register(user, client));
-            }
-            case CONNECT -> {
+                break;
+
+            case CONNECT:
                 user = server.getUserCollection().get(params.group("username"));
                 client.setUser(user);
                 client.sendMessage(connect(user.getLogin()));
-            }
-            case CONFIRM -> {
+                break;
+
+            case CONFIRM:
                 user = client.getUser();
                 String received = params.group("challenge");
                 String expected = user.getBcrypt().generateChallenge(client.getRandom22());
                 client.sendMessage(confirm(received, expected));
-            }
-            case MSG -> server.broadcastToAllClientsExceptMe(client, params.group("message"));
+                break;
+
+            case MSG:
+                server.broadcastToAllClientsExceptMe(client, params.group("message"));
+                break;
         }
     }
-
 
     private String connect(String login) {
         if (server.getUserCollection().containsKey(login)) {
@@ -100,11 +99,11 @@ public class Executor implements TaskScheduler {
     }
 
 
-    private String confirm(String clientChallenge, String userChallenge) {
+    private String confirm(String clientChallenge, String userChallenge){
         return clientChallenge.equals(userChallenge) ? "+OK" : "-ERR";
     }
 
-    private String register(User user, ClientRunnable client) {
+    private String register(User user, ClientRunnable client)  {
         try {
             server.registerUser(user);
             client.setUser(user);
