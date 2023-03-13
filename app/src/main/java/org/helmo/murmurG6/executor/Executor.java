3ebb0e0dbf98ca0303723c3479cd7bf1b0599e87
Giuseppe Franchina <l.loiseau@student.helmo.be>
2023-03-13 21:46:46 +0100

    commit

--- app/src/main/java/org/helmo/murmurG6/executor/Executor.java
+++ app/src/main/java/org/helmo/murmurG6/executor/Executor.java
@@ -1,6 +1,5 @@
 package org.helmo.murmurG6.executor;
 
-import org.helmo.murmurG6.controller.ClientRunnable;
 import org.helmo.murmurG6.models.Protocol;
 import org.helmo.murmurG6.controller.ServerController;
 import org.helmo.murmurG6.controller.TaskScheduler;
