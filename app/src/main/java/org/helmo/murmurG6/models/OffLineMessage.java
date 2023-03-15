a83e2730dbc7c6d8430bbca0a5fb4b2a4cd91866
Anthony UCCI <a.ucci@student.helmo.be>
2023-03-15 22:15:31 +0100

    Fix message offline double

--- app/src/main/java/org/helmo/murmurG6/models/OffLineMessage.java
+++ app/src/main/java/org/helmo/murmurG6/models/OffLineMessage.java
@@ -33,6 +33,15 @@ public class OffLineMessage{
     }
 
 
+    /*
+    Pour la création de l'id, il fallait opter pour un moyen d'obtenir un clé triable par date (plus ancien au plus récent).
+    Tout chiffrement de cet id pertureberait l'ordre du tri. Par conséquent, on crée un id qui est le resultat sous forme de
+    chaine de caractères de la concaténation de la date de traitement du message avec l'emetteur de ce message.
+    Créer un id uniquement sur base de la date aurait été problématique dans le cas ou le client recevrait deux message en
+    même temps.
+    Dans notre cas, on considèrera que seule l'information "message" est une information sensible à sécuriser. La date et l'émetteur
+    étant des informations non sensibles car les information d'utilisateur sont stockée en claire dans le serveur.
+     */
     public void setEncryptedMessage(String ciphertext_base64) {
         this.message = ciphertext_base64;
         this.messageId = (int) dateTime.toEpochSecond(ZoneOffset.UTC)+senderCreditentialsInString;
